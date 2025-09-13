ef643dccd964e91ea5709537bfffc43225909ae7
Ouhdane Chaimae <chaimaeouhdane@gmail.com>
2025-09-13 16:54:32 +0200

    node modules deleted and sharp added

--- node_modules/onnxruntime-common/lib/backend.ts
+++ node_modules/onnxruntime-common/lib/backend.ts
@@ -1,49 +1,64 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {InferenceSession} from './inference-session';
-import {OnnxValue} from './onnx-value';
+import { InferenceSession } from './inference-session.js';
+import { OnnxValue } from './onnx-value.js';
 
 /**
- * @internal
+ * @ignore
  */
 export declare namespace SessionHandler {
-  type FeedsType = {[name: string]: OnnxValue};
-  type FetchesType = {[name: string]: OnnxValue | null};
-  type ReturnType = {[name: string]: OnnxValue};
+  type FeedsType = { [name: string]: OnnxValue };
+  type FetchesType = { [name: string]: OnnxValue | null };
+  type ReturnType = { [name: string]: OnnxValue };
 }
 
 /**
- * Represent a handler instance of an inference session.
+ * Represents shared SessionHandler functionality
  *
- * @internal
+ * @ignore
  */
-export interface SessionHandler {
+interface SessionHandler {
   dispose(): Promise<void>;
 
   readonly inputNames: readonly string[];
   readonly outputNames: readonly string[];
 
+  readonly inputMetadata: readonly InferenceSession.ValueMetadata[];
+  readonly outputMetadata: readonly InferenceSession.ValueMetadata[];
+}
+
+/**
+ * Represent a handler instance of an inference session.
+ *
+ * @ignore
+ */
+export interface InferenceSessionHandler extends SessionHandler {
   startProfiling(): void;
   endProfiling(): void;
 
-  run(feeds: SessionHandler.FeedsType, fetches: SessionHandler.FetchesType,
-      options: InferenceSession.RunOptions): Promise<SessionHandler.ReturnType>;
+  run(
+    feeds: SessionHandler.FeedsType,
+    fetches: SessionHandler.FetchesType,
+    options: InferenceSession.RunOptions,
+  ): Promise<SessionHandler.ReturnType>;
 }
 
 /**
  * Represent a backend that provides implementation of model inferencing.
  *
- * @internal
+ * @ignore
  */
 export interface Backend {
   /**
    * Initialize the backend asynchronously. Should throw when failed.
    */
-  init(): Promise<void>;
+  init(backendName: string): Promise<void>;
 
-  createSessionHandler(uriOrBuffer: string|Uint8Array, options?: InferenceSession.SessionOptions):
-      Promise<SessionHandler>;
+  createInferenceSessionHandler(
+    uriOrBuffer: string | Uint8Array,
+    options?: InferenceSession.SessionOptions,
+  ): Promise<InferenceSessionHandler>;
 }
 
-export {registerBackend} from './backend-impl';
+export { registerBackend } from './backend-impl.js';
--- node_modules/onnxruntime-common/lib/env.ts
+++ node_modules/onnxruntime-common/lib/env.ts
@@ -1,14 +1,34 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import {EnvImpl} from './env-impl';
+import { env as envImpl } from './env-impl.js';
+import { TryGetGlobalType } from './type-helper.js';
+
 export declare namespace Env {
-  export type WasmPrefixOrFilePaths = string|{
-    'ort-wasm.wasm'?: string;
-    'ort-wasm-threaded.wasm'?: string;
-    'ort-wasm-simd.wasm'?: string;
-    'ort-wasm-simd-threaded.wasm'?: string;
-  };
+  export type WasmPathPrefix = string;
+  export interface WasmFilePaths {
+    /**
+     * Specify the override path for the main .wasm file.
+     *
+     * This path should be an absolute path.
+     *
+     * If not modified, the filename of the .wasm file is:
+     * - `ort-wasm-simd-threaded.wasm` for default build
+     * - `ort-wasm-simd-threaded.jsep.wasm` for JSEP build (with WebGPU and WebNN)
+     */
+    wasm?: URL | string;
+    /**
+     * Specify the override path for the main .mjs file.
+     *
+     * This path should be an absolute path.
+     *
+     * If not modified, the filename of the .mjs file is:
+     * - `ort-wasm-simd-threaded.mjs` for default build
+     * - `ort-wasm-simd-threaded.jsep.mjs` for JSEP build (with WebGPU and WebNN)
+     */
+    mjs?: URL | string;
+  }
+  export type WasmPrefixOrFilePaths = WasmPathPrefix | WasmFilePaths;
   export interface WebAssemblyFlags {
     /**
      * set or get number of thread(s). If omitted or set to 0, number of thread(s) will be determined by system. If set
@@ -21,13 +41,31 @@ export declare namespace Env {
     numThreads?: number;
 
     /**
-     * set or get a boolean value indicating whether to enable SIMD. If set to false, SIMD will be forcely disabled.
+     * set a value indicating whether to enable SIMD.
+     *
+     * ONNX Runtime will perform feature detection based on the value of this property. Specifically, when the value is
+     * set to:
+     * - `undefined`, `true` or `"fixed"`: will check availability of Fixed-width SIMD.
+     * - `"relaxed"`: will check availability of Relaxed SIMD.
+     * - `false`: will not perform SIMD feature checking.
+     *
+     * Setting this property does not make ONNX Runtime to switch to the corresponding runtime automatically. User need
+     * to set `wasmPaths` or `wasmBinary` property to load the corresponding runtime.
      *
      * This setting is available only when WebAssembly SIMD feature is available in current context.
      *
      * @defaultValue `true`
      */
-    simd?: boolean;
+    simd?: boolean | 'fixed' | 'relaxed';
+
+    /**
+     * set or get a boolean value indicating whether to enable trace.
+     *
+     * @defaultValue `false`
+     *
+     * @deprecated Use `env.trace` instead. If `env.trace` is set, this property will be ignored.
+     */
+    trace?: boolean;
 
     /**
      * Set or get a number specifying the timeout for initialization of WebAssembly backend, in milliseconds. A zero
@@ -38,10 +76,16 @@ export declare namespace Env {
     initTimeout?: number;
 
     /**
-     * Set a custom URL prefix to the .wasm files or a set of overrides for each .wasm file. The override path should be
-     * an absolute path.
+     * Set a custom URL prefix to the .wasm/.mjs files, or an object of overrides for both .wasm/.mjs file. The override
+     * path should be an absolute path.
      */
     wasmPaths?: WasmPrefixOrFilePaths;
+
+    /**
+     * Set a custom buffer which contains the WebAssembly binary. If this property is set, the `wasmPaths` property will
+     * be ignored.
+     */
+    wasmBinary?: ArrayBufferLike | Uint8Array;
 
     /**
      * Set or get a boolean value indicating whether to proxy the execution of main thread to a worker thread.
@@ -57,7 +101,11 @@ export declare namespace Env {
      *
      * @defaultValue `'webgl2'`
      */
-    contextId?: 'webgl'|'webgl2';
+    contextId?: 'webgl' | 'webgl2';
+    /**
+     * Get the WebGL rendering context.
+     */
+    readonly context: WebGLRenderingContext;
     /**
      * Set or get the maximum batch size for matmul. 0 means to disable batching.
      *
@@ -69,7 +117,7 @@ export declare namespace Env {
      *
      * @defaultValue `'full'`
      */
-    textureCacheMode?: 'initializerOnly'|'full';
+    textureCacheMode?: 'initializerOnly' | 'full';
     /**
      * Set or get the packed texture mode
      *
@@ -83,6 +131,114 @@ export declare namespace Env {
      */
     async?: boolean;
   }
+
+  export interface WebGpuProfilingDataV1TensorMetadata {
+    dims: readonly number[];
+    dataType: string;
+  }
+  export interface WebGpuProfilingDataV1 {
+    version: 1;
+    inputsMetadata: readonly WebGpuProfilingDataV1TensorMetadata[];
+    outputsMetadata: readonly WebGpuProfilingDataV1TensorMetadata[];
+    kernelId: number;
+    kernelType: string;
+    kernelName: string;
+    programName: string;
+    startTime: number;
+    endTime: number;
+  }
+
+  export type WebGpuProfilingData = WebGpuProfilingDataV1;
+
+  export interface WebGpuFlags {
+    /**
+     * Set or get the profiling mode.
+     *
+     * @deprecated Use `env.webgpu.profiling.mode` instead. If `env.webgpu.profiling.mode` is set, this property will be
+     * ignored.
+     */
+    profilingMode?: 'off' | 'default';
+    /**
+     * Set or get the profiling configuration.
+     */
+    profiling: {
+      /**
+       * Set or get the profiling mode.
+       *
+       * @defaultValue `'off'`
+       */
+      mode?: 'off' | 'default';
+
+      /**
+       * Set or get a callback function when a profiling data is received. If not set, the profiling data will be
+       * printed to console.
+       */
+      ondata?: (data: WebGpuProfilingData) => void;
+    };
+    /**
+     * Set or get the power preference.
+     *
+     * Setting this property only has effect before the first WebGPU inference session is created. The value will be
+     * used as options for `navigator.gpu.requestAdapter()`.
+     *
+     * See {@link https://gpuweb.github.io/gpuweb/#dictdef-gpurequestadapteroptions} for more details.
+     *
+     * @defaultValue `undefined`
+     *
+     * @deprecated Create your own GPUAdapter, use it to create a GPUDevice instance and set {@link device} property if
+     * you want to use a specific power preference.
+     */
+    powerPreference?: 'low-power' | 'high-performance';
+    /**
+     * Set or get the force fallback adapter flag.
+     *
+     * Setting this property only has effect before the first WebGPU inference session is created. The value will be
+     * used as options for `navigator.gpu.requestAdapter()`.
+     *
+     * See {@link https://gpuweb.github.io/gpuweb/#dictdef-gpurequestadapteroptions} for more details.
+     *
+     * @defaultValue `undefined`
+     *
+     * @deprecated Create your own GPUAdapter, use it to create a GPUDevice instance and set {@link device} property if
+     * you want to use a specific fallback option.
+     */
+    forceFallbackAdapter?: boolean;
+    /**
+     * Set or get the adapter for WebGPU.
+     *
+     * Setting this property only has effect before the first WebGPU inference session is created. The value will be
+     * used as the GPU adapter for the underlying WebGPU backend to create GPU device.
+     *
+     * If this property is not set, it will be available to get after the first WebGPU inference session is created. The
+     * value will be the GPU adapter that created by the underlying WebGPU backend.
+     *
+     * When use with TypeScript, the type of this property is `GPUAdapter` defined in "@webgpu/types".
+     *
+     * @deprecated It is no longer recommended to use this property. The latest WebGPU spec adds `GPUDevice.adapterInfo`
+     * (https://www.w3.org/TR/webgpu/#dom-gpudevice-adapterinfo), which allows to get the adapter information from the
+     * device. When it's available, there is no need to set/get the {@link adapter} property.
+     */
+    adapter: TryGetGlobalType<'GPUAdapter'>;
+    /**
+     * Set or get the GPU device for WebGPU.
+     *
+     * There are 3 valid scenarios of accessing this property:
+     * - Set a value before the first WebGPU inference session is created. The value will be used by the WebGPU backend
+     * to perform calculations. If the value is not a `GPUDevice` object, an error will be thrown.
+     * - Get the value before the first WebGPU inference session is created. This will try to create a new GPUDevice
+     * instance. Returns a `Promise` that resolves to a `GPUDevice` object.
+     * - Get the value after the first WebGPU inference session is created. Returns a resolved `Promise` to the
+     * `GPUDevice` object used by the WebGPU backend.
+     */
+    get device(): Promise<TryGetGlobalType<'GPUDevice'>>;
+    set device(value: TryGetGlobalType<'GPUDevice'>);
+    /**
+     * Set or get whether validate input content.
+     *
+     * @defaultValue `false`
+     */
+    validateInputContent?: boolean;
+  }
 }
 
 export interface Env {
@@ -91,7 +247,8 @@ export interface Env {
    *
    * @defaultValue `'warning'`
    */
-  logLevel?: 'verbose'|'info'|'warning'|'error'|'fatal';
+  logLevel?: 'verbose' | 'info' | 'warning' | 'error' | 'fatal';
+
   /**
    * Indicate whether run in debug mode.
    *
@@ -100,14 +257,37 @@ export interface Env {
   debug?: boolean;
 
   /**
+   * set or get a boolean value indicating whether to enable trace.
+   *
+   * @defaultValue `false`
+   */
+  trace?: boolean;
+
+  /**
+   * Get version of the current package.
+   */
+  readonly versions: {
+    readonly common: string;
+    readonly web?: string;
+    readonly node?: string;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    readonly 'react-native'?: string;
+  };
+
+  /**
    * Represent a set of flags for WebAssembly
    */
-  wasm: Env.WebAssemblyFlags;
+  readonly wasm: Env.WebAssemblyFlags;
 
   /**
    * Represent a set of flags for WebGL
    */
-  webgl: Env.WebGLFlags;
+  readonly webgl: Env.WebGLFlags;
+
+  /**
+   * Represent a set of flags for WebGPU
+   */
+  readonly webgpu: Env.WebGpuFlags;
 
   [name: string]: unknown;
 }
@@ -115,4 +295,4 @@ export interface Env {
 /**
  * Represent a set of flags as a global singleton.
  */
-export const env: Env = new EnvImpl();
+export const env: Env = envImpl;
